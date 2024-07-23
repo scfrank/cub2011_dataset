69e133457476bbdb0f6a8d41b8ac92da9361b617
Stella Frank <stella.frank@gmail.com>
2024-07-23 18:27:19 +0200

    Multimodal CUB dataset, with (cleaned) scott reed descriptions.

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 PyTorch dataset for CUB-200-2011 (http://www.vision.caltech.edu/visipedia/CUB-200-2011.html).
+
+# CUB 20 subset: use first 20 species only as a toy dataset
+
+# With Scott Reed descriptions, as fixed by [Franz Rieger](https://github.com/riegerfr))
+
+Data Instructions
+- Create a data/ directory
+- within data/ create a softlink to the no_oov_descriptions.json file created by the reigerfr repo.
+` no_oov_descriptions.json@	 --> /home/stella/datasets/reedscott_birds/cub_updated_descriptions/updated_dataset/no_oov_descriptions.json`
+
--- mm_cub.py
+++ mm_cub.py
@@ -4,13 +4,24 @@ from torchvision.datasets.folder import default_loader
 from torchvision.datasets.utils import download_url
 from torch.utils.data import Dataset
 
+"""
+Pytorch dataset for CUB with descriptions from Scott Reed.
 
-class Cub2011(Dataset):
+Downloading CUB images is not allowed here because descriptions need to be set up as well.
+in /data/no_oov_descriptions.json
+
+Assume ./data
+
+"""
+
+class CubCaps(Dataset):
     base_folder = 'CUB_200_2011/images'
     # url = 'http://www.vision.caltech.edu/visipedia-data/CUB-200-2011/CUB_200_2011.tgz'
     url = "https://data.caltech.edu/records/65de6-vp158/files/CUB_200_2011.tgz?download=1"
     filename = 'CUB_200_2011.tgz'
     tgz_md5 = '97eceeb196236b17998738112f37df78'
+
+    descriptions_file = 'no_oov_descriptions.json'
 
     def __init__(self, root, train=True, transform=None, loader=default_loader, download=True):
         self.root = os.path.expanduser(root)
@@ -34,7 +45,14 @@ class Cub2011(Dataset):
                                        sep=' ', names=['img_id', 'is_training_img'])
 
         data = images.merge(image_class_labels, on='img_id')
-        self.data = data.merge(train_test_split, on='img_id')
+        data = data.merge(train_test_split, on='img_id')
+
+        captions = pd.read_json(os.path.join(self.root, self.descriptions_file)).T  # $IMGs rows x 10 cols
+        captions['img_id'] = captions.reset_index().index + 1
+
+        captions_long = captions.melt(id_vars='img_id', var_name='desc_id', value_name='description')
+
+        self.data = data.merge(captions_long, on='img_id')
 
         if self.train:
             self.data = self.data[self.data.is_training_img == 1]
@@ -75,7 +93,13 @@ class Cub2011(Dataset):
         target = sample.target - 1  # Targets start at 1 by default, so shift to 0
         img = self.loader(path)
 
+        description = sample.description
+
         if self.transform is not None:
             img = self.transform(img)
 
-        return img, target
+        return (img, description), target
+
+if __name__ == "__main__":
+    cc=CubCaps('./data', download=False)
+    print("first", cc[0])
